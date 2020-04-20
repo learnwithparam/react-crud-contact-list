6da8328c145b1ae74aa945875645442c47ae2a22
Param Harrison <paramanantham@live.com>
2020-04-20 21:15:16 +0300

    feat: Exercise: Handle non exiting routes using React Router

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ContactList from "./components/contact-list";
 import AddContact from "./components/add-contact";
 import EditContact from "./components/edit-contact";
 import Header from "./components/header";
+import NotFound from "./components/not-found";
 import GlobalProvider from "./components/global-state";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" component={ContactList} exact />
         <Route path="/add" component={AddContact} />
         <Route path="/edit/:id" component={EditContact} />
+        <Route component={NotFound} />
       </Switch>
     </GlobalProvider>
   );
